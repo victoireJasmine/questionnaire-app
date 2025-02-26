d9c252f33ef6987c6ba386152ff3c137436fba79
victoirejasmine <viejasmine0@gmail.com>
2025-02-26 11:32:51 +0100

    server config

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.router.stack.forEach((r) => {
-  if (r.route && r.route.path) {
-    console.log("route chargée:" , r.route.path);
-  }
-});
-
+// Définition des routes
 app.use("/api/questionnaires", require("./routes/questionnaireRoutes"));
 app.use("/api/questionnaires", require("./routes/questionRoutes"));
+
+// Afficher les routes chargées (en utilisant app._router)
+app._router.stack.forEach((middleware) => {
+  if (middleware.route && middleware.route.path) {
+    console.log("Route chargée:", middleware.route.path);
+  }
+});
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
