123eeaa9a44c4d5ae67f83ae47663ddf853f376c
victoirejasmine <viejasmine0@gmail.com>
2025-02-26 17:18:22 +0100

    config acces bdd

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log(" MongoDB connecté !");
+    let mongoUri = process.env.MONGO_URI;
+
+    if (process.env.NODE_ENV === "test") {
+      console.log("🚀 Utilisation de MongoMemoryServer pour les tests...");
+      const mongoServer = await MongoMemoryServer.create();
+      mongoUri = mongoServer.getUri();
+    }
+
+    await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("✅ MongoDB connecté !");
   } catch (error) {
-    console.error(" Erreur MongoDB :", error);
+    console.error("❌ Erreur MongoDB :", error);
     process.exit(1);
   }
 };
